099d6a64b6b5995a0c6bea7aba5b6e6eb6036750
bingo-game <friendstech45@gmail.com>
2024-06-20 21:35:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<script>
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -112,3 +113,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+</script>
